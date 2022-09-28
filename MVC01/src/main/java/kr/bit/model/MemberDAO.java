5bd5f8c044c000a0a1cbea2c6fa913b100785e53
j2nhyeok <103464891+j2nhyeok@users.noreply.github.com>
2022-09-28 22:07:40 +0900

    Add files via upload

--- MVC01/src/main/java/kr/bit/model/MemberDAO.java
+++ MVC01/src/main/java/kr/bit/model/MemberDAO.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class MemberDAO {
 	private Connection conn;
@@ -45,12 +47,54 @@ public class MemberDAO {
 			ps.setString(6, vo.getPhone());
 			
 			// 성공하면 1, 실패하면 0
-			 cnt = ps.executeUpdate(); //전송(실행)
-		} catch (Exception e) {
+			 cnt = ps.executeUpdate(); //전송(실행), insert경우 추가되는 것이므로, update가 되어야 함.
+		}catch (Exception e) {
 			e.printStackTrace();
+		}finally {
+			dbClose();
 		}
 		
 		return cnt; 
+	}
+	
+	// 회원(VO) 전체 리스트(ArrayList) 가져오기
+	public ArrayList<MemberVO> memberList() {
+		String SQL = "SELECT * FROM member";
+		getConnect();
+		ArrayList<MemberVO> list = new ArrayList<MemberVO>();
+		try {
+			ps = conn.prepareStatement(SQL);
+			rs = ps.executeQuery(); // 데이터베이스에 변화가 없으므로 update X. query O.  / rs => 커서
+			
+			while(rs.next()) {
+				int num = rs.getInt("num");
+				String id = rs.getString("id");
+				String pass = rs.getString("pass");
+				String name = rs.getString("name");
+				int age = rs.getInt("age");
+				String email = rs.getString("email");
+				String phone = rs.getString("phone");
+				
+				MemberVO vo = new MemberVO(num, id, pass, name, age, email, phone);
+				list.add(vo);
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			dbClose();
+		}
 
+		return list;
+	}
+	// 데이터베이스 연결 끊기
+	public void dbClose() {
+		try {
+			if(rs!=null) rs.close();
+			if(ps!=null) ps.close();
+			if(conn!=null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
