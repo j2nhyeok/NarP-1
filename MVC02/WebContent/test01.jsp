a81dcdb1d820ff8e1f97401e381bf6a782327b55
j2nhyeok <103464891+j2nhyeok@users.noreply.github.com>
2022-10-03 20:58:16 +0900

    Add files via upload

---
+++ MVC02/WebContent/test01.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%> <!-- jsp문서임을 선언하는 '지시자' -->
+<%-- jsp 주석 --%>
+<%!
+	public int hap(int s, int e){
+		int sum=0;
+		for(int i=s;i<=e;i++){
+			sum+=i;
+		}
+		
+		return sum;
+	}
+%> <!-- jsp에서 자바함수 사용하기 위한 '선언문' -->
+<%     
+	int sum=0;
+	for(int i=1;i<=100;i++){
+		sum+=i;
+	}
+%> <!-- 자바 코드를 사용하기 위한 '스크립트릿'    -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+55부터 350까지의 총합=<%= hap(55,350) %>  <!-- 자바의 변수를 출력하기 위한 '출력식'  -->
+</body>
+</html>
---
+++ MVC02/src/main/java/kr/bit/model/MemberDAO.java
@@ -0,0 +1,172 @@
+package kr.bit.model;
+// JDBC -> MyBatis, JPA
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class MemberDAO {
+	private Connection conn;
+	private PreparedStatement ps;
+	private ResultSet rs;
+
+	// 데이터베이스 연결 객체 생성
+	public void getConnect() {
+		// 데이터 베이스 접속 URL
+		String URL="jdbc:mysql://localhost:3306/test?characterEncoding=UTF-8&serverTimezone=UTC";
+		String user="root";
+		String password="";
+
+		// MySQL Driver Loading
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn=DriverManager.getConnection(URL, user, password); // MySQL 에 접속을 시도, 성공하면 연결정보를 넘겨줌
+		} catch (Exception e) {
+			e.printStackTrace();
+		} // 동적 로딩(실행 시점에서 객체를 생성하는 방법)
+	}
+
+	// 회원 저장 동작
+	public int memberInsert(MemberVO vo) {
+		//																 ?(파라메터) 1, 2, 3, 4, 5, 6
+		String SQL="INSERT INTO member(id, pass, name, age, email, phone) values(?, ?, ?, ?, ?, ?)";
+		getConnect();
+		
+		// SQL문장을 전송하는 객체 생성
+		int cnt=-1;
+		try {
+			ps=conn.prepareStatement(SQL); // MySQL에서 미리 컴파일을 시킨다. -> 미리 컴파일 시키기 때문에 속도가 빨라진다.
+			ps.setString(1, vo.getId());
+			ps.setString(2, vo.getPass());
+			ps.setString(3, vo.getName());
+			ps.setInt(4, vo.getAge());
+			ps.setString(5, vo.getEmail());
+			ps.setString(6, vo.getPhone());
+			
+			// 성공하면 1, 실패하면 0
+			 cnt=ps.executeUpdate(); //전송(실행), insert경우 추가되는 것이므로, update가 되어야 함.
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			dbClose();
+		}
+		
+		return cnt; 
+	}
+	
+	// 회원(VO) 전체 리스트(ArrayList) 가져오기
+	public ArrayList<MemberVO> memberList() {
+		String SQL="SELECT * FROM member";
+		getConnect();
+		ArrayList<MemberVO> list=new ArrayList<MemberVO>();
+		try {
+			ps=conn.prepareStatement(SQL);
+			rs=ps.executeQuery(); // 데이터베이스에 변화가 없으므로 update X. query O.  / rs => 커서
+			
+			while(rs.next()) {
+				int num=rs.getInt("num");
+				String id=rs.getString("id");
+				String pass=rs.getString("pass");
+				String name=rs.getString("name");
+				int age=rs.getInt("age");
+				String email=rs.getString("email");
+				String phone=rs.getString("phone");
+				
+				MemberVO vo=new MemberVO(num, id, pass, name, age, email, phone);
+				list.add(vo);
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			dbClose();
+		}
+
+		return list;
+	}
+	
+	// 특정 회원 삭제
+	public int memberDelete(int num) {
+		String SQL="DELETE FROM member WHERE num=?";
+		getConnect();
+		int cnt=-1;
+		try {
+			ps=conn.prepareStatement(SQL);
+			ps.setInt(1, num);
+			cnt=ps.executeUpdate();
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			dbClose();
+		}
+		return cnt;
+	}
+	
+	// 특정 멤버 한 명의 세부 정보 가져오기
+	public MemberVO memberContent(int num) {
+		String SQL="SELECT * FROM member WHERE num=?";
+		getConnect();
+		MemberVO vo = null;
+		try {
+			ps=conn.prepareStatement(SQL);
+			ps.setInt(1, num);
+			
+			rs=ps.executeQuery();
+			if(rs.next()) {
+				num=rs.getInt("num");
+				String id=rs.getString("id");
+				String pass=rs.getString("pass");
+				String name=rs.getString("name");
+				int age=rs.getInt("age");
+				String email=rs.getString("email");
+				String phone=rs.getString("phone");
+				
+				vo=new MemberVO(num, id, pass, name, age, email, phone);
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}finally {
+			dbClose();
+		}
+		return vo;
+	}
+	
+	// 특정 멤버 정보 수정
+	public int memberUpdate(MemberVO vo) {
+		String SQL="UPDATE member SET age=?, email=?, phone=? WHERE num=?";
+		getConnect();
+		int cnt=-1;
+		try {
+			ps=conn.prepareStatement(SQL);
+			ps.setInt(1, vo.getAge());
+			ps.setString(2, vo.getEmail());
+			ps.setString(3, vo.getPhone());
+			ps.setInt(4, vo.getNum());
+			
+			cnt=ps.executeUpdate();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			dbClose();
+		}
+		return cnt;
+	}
+	
+	// 데이터베이스 연결 끊기
+	public void dbClose() {
+		try {
+			if(rs!=null) rs.close();
+			if(ps!=null) ps.close();
+			if(conn!=null) conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
